3aead8b138a251ddbbdd9a90829f7edd7ed97b0c
Italo Barboza <italobarboza070@gmail.com>
2025-03-31 19:41:17 -0300

    feat: change url to fetch api in web

--- apps/web/src/lib/apollo.ts
+++ apps/web/src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'http://app.clarke.fornecedores.listaperfeita.com/graphql',
+  uri: 'https://appclarkefornecedores.listaperfeita.com/graphql',
   cache: new InMemoryCache(),
 });
